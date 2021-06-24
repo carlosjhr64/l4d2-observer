3878e5144676a5f1721b8a43070bc5a7b7c21de1
carlosjhr64 <carlosjhr64@gmail.com>
2021-06-23 18:51:17 -0700

    bug fix balance_rankings

--- lib/l4d2-observer/observer.rb
+++ lib/l4d2-observer/observer.rb
@@ -105,7 +105,7 @@ class Observer
         cmd = kickid(id, 'kicked for name registration issue')
       when TrueClass
         PUTS.terminal line, :yellow
-        SURVIVOR.balance_rankings
+        SURVIVOR.balance_rankings(survivor)
         if SURVIVOR.kicks(survivor) > EXCESSIVE_KICKS
           cmd = say "#{survivor} is a troll!"
         else
