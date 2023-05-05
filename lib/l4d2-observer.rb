35ee173f4ac844ffe71c54b73bd517d41b1e3a9d
CarlosJHR64 <carlosjhr64@gmail.com>
2023-05-04 18:00:34 -0700

    rm redundant .rb

--- lib/l4d2-observer.rb
+++ lib/l4d2-observer.rb
@@ -4,7 +4,7 @@ require 'yaml'
 ### Gems ###
 require 'rainbow/refinement'
 ### local ###
-require_relative 'l4d2-observer/puts.rb'
-require_relative 'l4d2-observer/survivor.rb'
-require_relative 'l4d2-observer/observer.rb'
-require_relative 'l4d2-observer/run.rb'
+require_relative 'l4d2-observer/puts'
+require_relative 'l4d2-observer/survivor'
+require_relative 'l4d2-observer/observer'
+require_relative 'l4d2-observer/run'
