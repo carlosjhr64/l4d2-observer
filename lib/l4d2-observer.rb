a78eb6ec5246a710c6601631bc8723be966fca07
CarlosJHR64 <carlosjhr64@gmail.com>
2023-05-05 07:58:33 -0700

    Catch missing gems early.

--- lib/l4d2-observer.rb
+++ lib/l4d2-observer.rb
@@ -1,8 +1,3 @@
-### Standard Library ###
-require 'pty'
-require 'yaml'
-### Gems ###
-require 'rainbow/refinement'
 ### local ###
 require_relative 'l4d2-observer/puts'
 require_relative 'l4d2-observer/survivor'
